b2dbe2e436af8a81d160702268ea82a61a6fd613
Kenneth Quintero <kennethgqv@gmail.com>
2023-01-26 01:32:49 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Client } from "../deps.ts";
 // Consider that for a private page to be compiled into html you would need to
 // use the notion api and an integration token to query it. This is not the case
 // for public pages where you only need to make an http request to the url of the
@@ -5,4 +6,9 @@
 
 // First I gonna start with the private page because that's what i'm currently working
 // with.
-async function notionPageToHtml(pageId: string): Promise<string> {}
+async function notionPageToHtml(
+  pageId: string,
+  notion: Client
+): Promise<string> {
+  const children = await notion.blocks.children.list({ block_id: pageId });
+}
