549332cf8754c9fde32d590da538f7fd2b70eb1c
Kenneth Quintero <kennethgqv@gmail.com>
2023-01-26 03:21:25 -0700

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,2 +1,5 @@
 export { Client } from "https://deno.land/x/notion_sdk/src/mod.ts";
-export { type BlockObjectResponse } from "https://deno.land/x/notion_sdk@v1.0.4/src/api-endpoints.ts";
+export {
+  type BlockObjectResponse,
+  type RichTextItemResponse,
+} from "https://deno.land/x/notion_sdk@v1.0.4/src/api-endpoints.ts";
